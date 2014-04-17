af53deeba3d2abeffaea2c59dd39b825979ce166
greysonzhou <zhouxingxing_ok_12@163.com>
2014-04-18 00:29:07 +0800

    后台整好

--- DOC/数据库更改_周告生.sql
+++ DOC/数据库更改_周告生.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `tp_item` ADD `favi` INT( 10) NOT NULL DEFAULT'0' COMMENT'收藏数目';
 ALTER TABLE `tp_item_taobao`ADD`item_model` INT( 4)NOT NULL AFTER `Huohao` ;
 ALTER TABLE  `tp_item` ADD  `detail_stock` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL AFTER  `images` ;
+
+ALTER TABLE  `tp_item` CHANGE  `intro`  `intro` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL ;
