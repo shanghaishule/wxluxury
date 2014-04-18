e5365a40f69ecdc197dcd42dd79a68c3e3511832
greysonzhou <zhouxingxing_ok_12@163.com>
2014-04-18 12:42:58 +0800

    细则库存完成了

--- DOC/数据库更改_周告生.sql
+++ DOC/数据库更改_周告生.sql
@@ -3,3 +3,4 @@ ALTER TABLE `tp_item_taobao`ADD`item_model` INT( 4)NOT NULL AFTER `Huohao` ;
 ALTER TABLE  `tp_item` ADD  `detail_stock` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL AFTER  `images` ;
 
 ALTER TABLE  `tp_item` CHANGE  `intro`  `intro` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL ;
+ALTER TABLE  `tp_order_detail` CHANGE  `size`  `size` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  '';
