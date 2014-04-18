142c4a15da930b86f3a05828d82e179379c0701a
greysonzhou <zhouxingxing_ok_12@163.com>
2014-04-19 00:43:59 +0800

    用户实体店申请

--- DOC/数据库更改_周告生.sql
+++ DOC/数据库更改_周告生.sql
@@ -4,3 +4,6 @@ ALTER TABLE  `tp_item` ADD  `detail_stock` TEXT CHARACTER SET utf8 COLLATE utf8_
 
 ALTER TABLE  `tp_item` CHANGE  `intro`  `intro` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL ;
 ALTER TABLE  `tp_order_detail` CHANGE  `size`  `size` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  '';
+
+/*店铺的冻结状态*/
+ALTER TABLE  `tp_wecha_shop` ADD  `frozen` INT( 1 ) NOT NULL DEFAULT  '0' COMMENT  '0-冻结 1-正常 2-审核中' AFTER  `latitude` ;
