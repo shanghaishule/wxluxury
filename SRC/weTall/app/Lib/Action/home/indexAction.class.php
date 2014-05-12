d4a2d7771dd8f09d6f4909cfa189fb746ed2243e
greysonzhou <zhouxingxing_ok_12@163.com>
2014-05-12 20:22:50 +0800

    导航

--- SRC/weTall/app/Lib/Action/home/indexAction.class.php
+++ SRC/weTall/app/Lib/Action/home/indexAction.class.php
@@ -336,7 +336,7 @@ class indexAction extends frontendAction {
 	    		foreach ($endPoint as $end){    
 	    			if ($end["lbs_addr"] != "") {		
 	    				$end["nearJuli"] = $this->GetDistance($latitude,$longitude,$end["lat"],$end["longtitude"]);
-	    				//echo $end["nearJuli"]."--0".$latitude."----1<br>".$longitude."----2<br>".$end["lat"]."----3<br>".$end["longtitude"];die();
+	    				echo $end["nearJuli"]."--0".$latitude."----1<br>".$longitude."----2<br>".$end["lat"]."----3<br>".$end["longtitude"]."===".$end["nearJuli"];die();
 	    				$nearShop[] = $end;
 	    			}				
 	    		}
