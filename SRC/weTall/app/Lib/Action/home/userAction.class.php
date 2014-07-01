9b84c1e263bcb115742d40dcb76e16b1f996db9a
ranyigit <ranyi_num@163.com>
2014-07-01 18:30:25 +0800

    test

--- SRC/weTall/app/Lib/Action/home/userAction.class.php
+++ SRC/weTall/app/Lib/Action/home/userAction.class.php
@@ -247,7 +247,7 @@ class userAction extends userbaseAction {
     	$userinfo['last_login_time']=time(); 
     	$userinfo['last_login_ip']=get_client_ip();
     	echo $userinfo['openid'];
-    	$Userarr= M('user')->where("openid='".$userinfo['openid']."'")->find();
+    	$Userarr= M('user')->where(array('openid'=>$userinfo['openid']))->find();
     	dump($Userarr);
     	if(!empty($Userarr) && $Userarr!=''){
     		$_SESSION['uid']=$Userarr['id'];
