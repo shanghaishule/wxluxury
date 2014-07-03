738a27cdfa2023f2adeda230aa501b12400ec978
polo2013 <paul.zhou@163.com>
2014-07-03 22:15:12 +0800

    表结构修改记录

---
+++ DOC/数据库更改_周成波.sql
@@ -0,0 +1,10 @@
+-- 处理admin登录不了的问题
+ALTER TABLE `tp_user` ADD  `username` varchar(50) NOT NULL;
+ALTER TABLE `tp_user` ADD  `password` char(32) NOT NULL;
+ALTER TABLE `tp_user` ADD  `role` smallint(6) unsigned NOT NULL COMMENT '组ID';
+ALTER TABLE `tp_user` ADD  `status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '状态 1:启用 0:禁止';
+ALTER TABLE `tp_user` ADD  `remark` varchar(255) DEFAULT NULL COMMENT '备注说明';
+ALTER TABLE `tp_user` ADD  `last_location` varchar(100) DEFAULT NULL COMMENT '最后登录位置';
+ALTER TABLE `tp_user` ADD  `email` varchar(90) NOT NULL DEFAULT '';
+  
+update `tp_user` set `username` = 'admin', `password` = 'b80c4de605487af2bf83a7cbd1d68025', `role` = '5', `status` = '1' where id = 1;
