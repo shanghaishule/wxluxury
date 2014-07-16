9934db63715f04037f89345814ffe1afaf091af1
ranyigit <ranyi_num@163.com>
2014-07-16 09:31:42 +0800

    官方旗舰店

--- SRC/weTall/app/Lib/Action/home/indexAction.class.php
+++ SRC/weTall/app/Lib/Action/home/indexAction.class.php
@@ -751,7 +751,7 @@ class indexAction extends frontendAction {
     	$brand2["BelongBrand"] = $item_taobao["brand"];
     	//$wecha_shop = M("wecha_shop")->where($brand2)->select();
         
-    	$result = M()->table(array("item"=>"i","wecha_shop"=>"w"))->field("i.*,w.*")->where("i.tokenTall=w.tokenTall AND i.Huohao='".$Huohao["Huohao"]."' AND w.BelongBrand='".$brand2["BelongBrand"]."'")->order("w.level asc")->select();
+    	$result = M()->table(array("tp_item"=>"i","tp_wecha_shop"=>"w"))->field("i.*,w.*")->where("i.tokenTall=w.tokenTall AND i.Huohao='".$Huohao["Huohao"]."' AND w.BelongBrand='".$brand2["BelongBrand"]."'")->order("w.level asc")->select();
     	dump($result);exit;
     	$this->assign("item",$item_huohao);
     	$this->assign("wecha_shop",$wecha_shop);
