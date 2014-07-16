b764e50dd5c62c32a3ddf1f1e6cfc36909eb2c6a
ranyigit <ranyi_num@163.com>
2014-07-16 09:32:36 +0800

    官方旗舰店

--- SRC/weTall/app/Lib/Action/home/indexAction.class.php
+++ SRC/weTall/app/Lib/Action/home/indexAction.class.php
@@ -753,8 +753,8 @@ class indexAction extends frontendAction {
         
     	$result = M()->table(array("tp_item"=>"i","tp_wecha_shop"=>"w"))->field("i.*,w.*")->where("i.tokenTall=w.tokenTall AND i.Huohao='".$Huohao["Huohao"]."' AND w.BelongBrand='".$brand2["BelongBrand"]."'")->order("w.level asc")->select();
     	dump($result);exit;
-    	$this->assign("item",$item_huohao);
-    	$this->assign("wecha_shop",$wecha_shop);
+    	//$this->assign("item",$item_huohao);
+    	//$this->assign("wecha_shop",$wecha_shop);
     	$this->assign("item_taobao",$item_taobao);
     	$this->assign("title","全网比价");
     	$this->assign("brand",$brand_data);
