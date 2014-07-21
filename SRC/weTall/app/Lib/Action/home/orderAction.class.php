5b38043b742c1b5c3156bf710b9c21754e6ebc16
ananhenan <rhceabh@126.com>
2014-07-21 16:00:22 +0800

    测试22

--- SRC/weTall/app/Lib/Action/home/orderAction.class.php
+++ SRC/weTall/app/Lib/Action/home/orderAction.class.php
@@ -194,7 +194,7 @@ class orderAction extends userbaseAction {
 		$config['appId'] = "wx3079f89b18863917";
 		$config['appSecret'] = "69289876b8d040b3f9a367c80f8754c8";
 		if(!isset($_SESSION['uid']) || $_SESSION['uid']==''){
-			if (isset($_GET['code'])){
+			//if (isset($_GET['code'])){
 				//echo $_GET['code'].'--';
 				$Oauth = new Oauth2();
 				$userinfo=$Oauth->getUserinfo($_GET['code'],$config);
@@ -210,9 +210,9 @@ class orderAction extends userbaseAction {
 					$_SESSION['name']=$userinfo['nickname'];
 				}
 				// dump($_SESSION['uid'].'-1-'.$_SESSION['name']);exit;
-			}else{
-				$this->error('页面异常',"{:U(home/index/brandshop)}");
-			}
+			//}else{
+			//	$this->error('页面异常',"{:U(home/index/brandshop)}");
+			//}
 			 
 		}
 		$tokenTall = $this->getTokenTall();
