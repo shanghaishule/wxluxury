44a5d7814ad3388be17390bf073260e2165c83a0
ranyigit <ranyi_num@163.com>
2014-07-28 14:04:52 +0800

    test

--- SRC/weTall/app/Lib/Action/home/userAction.class.php
+++ SRC/weTall/app/Lib/Action/home/userAction.class.php
@@ -1018,6 +1018,7 @@ class userAction extends userbaseAction {
     				$this->assign('flag','1');//编辑
     				$this->assign("uInfo",$userinfo);
     			}
+    			dump($userinfo);exit;
     			$this->display();
     		}else{
     			$this->error("服务器繁忙！");
