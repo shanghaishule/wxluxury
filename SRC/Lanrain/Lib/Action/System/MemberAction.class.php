a16e4e9be51ae44119d60f7200871114fc09a035
ranyigit <ranyi_num@163.com>
2014-08-05 15:47:26 +0800

    test

--- SRC/Lanrain/Lib/Action/System/MemberAction.class.php
+++ SRC/Lanrain/Lib/Action/System/MemberAction.class.php
@@ -13,6 +13,19 @@ class MemberAction extends BackAction
     	$this->_list($mod);
     	$this->display();
     }
+    //会员详情
+   public function detailInfo(){
+   	   $where['id'] = $this->_get('id','trim');
+   	   $userInfo = M('user_info')->where($where)->find();
+   	   if(empty($userInfo)){
+   	   	   $this->error("该用户还未填详细信息");
+   	   }else{
+   	   	   $this->assign('userInfo',$userInfo);
+   	   	   dump($userInfo);die();
+   	   	   $this->display();
+   	   }
+   }
+    
    public function delete(){//会员管理删除会员
    	$mod =M('user');
    	$ids = trim($this->_request('id'), ',');
