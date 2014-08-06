add7b7631636b53f7f95d83397ccc356dfe03897
ranyigit <ranyi_num@163.com>
2014-08-06 11:21:53 +0800

    test

---
+++ SRC/weTall/app/Lib/Action/admin/memberAction.class.php
@@ -0,0 +1,20 @@
+<?php
+class memberAction extends backendAction{
+	public function _initialize(){
+		parent::_initialize();
+		$this->_mod=D('user_info');
+	}
+	public function edit(){
+		$where['uid'] = $this->_get('id','trim');
+		$userInfo = $this->_mod->where($where)->find();
+		$this->assign('open_validator', true);
+		$this->assign('userInfo',$userInfo);
+		if (IS_AJAX) {
+			$response = $this->fetch();
+			$this->ajaxReturn(1, '', $response);
+		} else {
+			$this->display();
+		}
+	}
+}
+?>
