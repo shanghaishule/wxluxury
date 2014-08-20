f7ff3b259207dd135dc006b63800617844f6f3b7
ranyigit <ranyi_num@163.com>
2014-08-20 10:19:55 +0800

    test

--- SRC/weTall/app/Lib/Action/home/userInfoAction.class.php
+++ SRC/weTall/app/Lib/Action/home/userInfoAction.class.php
@@ -1,32 +1,42 @@
 <?php
 class userInfoAction{
-	//返回用户信息
-	public function index(){
-		$redirecturl = urlencode("http://www.kuyimap.com/weTall/index.php?g=home&m=userInfo&a=returnUserInfo");
-		$url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx3079f89b18863917&redirect_uri=".$redirecturl."&response_type=code&scope=snsapi_base&state=123#wechat_redirect";
-		//$url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx3079f89b18863917&redirect_uri=".$redirecturl."&response_type=code&scope=snsapi_userinfo&state=zcb#wechat_redirect";
-		header("Location: ".$url);		 
-	}
-	
-	public function returnUserInfo(){
-		import('Think.ORG.Oauth2');
+	function getUserinfo($code,$config){
+	    //echo $_GET['code'];
 		$config['appId'] = "wx3079f89b18863917";
 		$config['appSecret'] = "69289876b8d040b3f9a367c80f8754c8";
-		if (isset($_GET['code'])){
-			//echo $_GET['code'].'--';
-			$Oauth = new Oauth2();
-			$userinfo=$Oauth->getUserinfo($_GET['code'],$config);
-			//dump(json_encode($userinfo));
-			echo json_encode($userinfo);
-		}else{
-			echo 'no code';
-		}
-		
+		$url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$config['appId'].'&secret='.$config['appSecret'].'&code='.$code.'&grant_type=authorization_code';
+		$request =$this->curlGet($url);
+		$requestArray = json_decode($request, true);
+		$accessToken = $this->getAccessToken($config);
+		$url2 = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=".$accessToken."&openid=".$requestArray['openid'];
+		$request2 = $this->curlGet($url2);
+		$requestArray2 = json_decode($request2, true);
+		header('Content-Type:text/html;charset=utf-8');
+		//var_dump($requestArray2);
+		return $requestArray2;
 	}
-	public function test(){
-		$userinfo = $this->index();
-		echo json_encode($userinfo);		
+	function curlGet($url){
+		$ch = curl_init();
+		$header = "Accept-Charset:utf-8";
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$temp = curl_exec($ch);
+		curl_close($ch);
+		return $temp;
 	}
+	
+	function getAccessToken($config) {
+	
+        $request =$this->curlGet('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' . $config['appId'] . '&secret=' . $config['appSecret']);
+		$requestArray = json_decode($request, true);
+        if (isset($requestArray['errcode'])) {
+            return false;
+        }
+        $accessToken = $requestArray['access_token'];
+        return $accessToken;
+    }
 }
 
 ?>
