e8343e0f613ff805fd05209b7fb8b2424d47e3ce
ranyigit <ranyi_num@163.com>
2014-10-11 10:59:06 +0800

    test

--- SRC/Lanrain/Lib/Action/Wap/IndexAction.class.php
+++ SRC/Lanrain/Lib/Action/Wap/IndexAction.class.php
@@ -123,14 +123,14 @@ class IndexAction extends BaseAction{
 		//$res=$this->convertLinks($res);
 		//dump($res);exit;
 		//$list = $db->field('uptatetime')->where($where)->group('uptatetime')->select();
-		$list = $db->query("select FROM_UNIXTIME(uptatetime,'%Y-%m-%d') as time from tp_img GROUP BY time order by time DESC ");
+		$list = $db->query("select FROM_UNIXTIME(uptatetime,'%Y-%m-%d') as time from tp_img where token = '".$this->_get('token','trim')."' and classid = '".$this->_get('classid','intval')."' GROUP BY time order by time DESC ");
 		$array = array();
 		$index= 0;
 		foreach($list as $key => $val){
 		  // $condition['classid'] = $where['classid'];
 		  // $condition['uptatetime'] = $val['time'];
 		  // $res=$db->where($condition)->order('uptatetime DESC,id DESC')->limit("{$p},".$pageSize."")->select();
-		   $res = $db->query("select *,FROM_UNIXTIME(uptatetime,'%Y-%m-%d') as time from tp_img where classid = '".$where['classid']."' and time = '".$val['time']."' limit '".$p."','".$pageSize."'");
+		   $res = $db->query("select * from tp_img where classid = '".$where['classid']."' and FROM_UNIXTIME(uptatetime,'%Y-%m-%d') = '".$val['time']."' limit {$p},{$pageSize}");
 		   $res=$this->convertLinks($res);
 		   $array[$index]['lists'] = $res;
 		   $index++;
