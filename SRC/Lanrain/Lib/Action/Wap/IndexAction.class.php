61c40c56eaa795f278fc677d16309e5a849f8877
ranyigit <ranyi_num@163.com>
2014-10-11 10:41:03 +0800

    test

--- SRC/Lanrain/Lib/Action/Wap/IndexAction.class.php
+++ SRC/Lanrain/Lib/Action/Wap/IndexAction.class.php
@@ -123,7 +123,7 @@ class IndexAction extends BaseAction{
 		//$res=$this->convertLinks($res);
 		//dump($res);exit;
 		//$list = $db->field('uptatetime')->where($where)->group('uptatetime')->select();
-		$list = $db->query("select FROM_UNIXTIME(uptatetime,'%Y-%m-%d') as time from tp_img group by time order by time desc )");
+		$list = $db->query("select FROM_UNIXTIME(uptatetime,'%Y-%m-%d') as time from tp_img GROUP BY time order by time DESC )");
 		$array = array();
 		$index= 0;
 		foreach($list as $key => $val){
