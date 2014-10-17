f927c02b886fa774bfb3d6de3dd7d2fc0c78008a
ranyigit <ranyi_num@163.com>
2014-10-17 15:41:28 +0800

    test

--- SRC/Lanrain/Lib/Action/System/DatafromShopAction.class.php
+++ SRC/Lanrain/Lib/Action/System/DatafromShopAction.class.php
@@ -44,7 +44,7 @@ class DatafromShopAction extends BackAction
     	}
     	$this->assign('list',$pageData);
     	$this->assign('page',$show);// 赋值分页输出
-    	$this->assign('brand',$_SESSION['brand']);// 赋值分页输出
+    	$this->assign('brand',$brandname);// 赋值分页输出
     	$this->assign('shop_name',$shop_name);// 赋值分页输出
     	
     	$brandlist = M("brandlist");
