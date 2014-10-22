d9f80d1669d8be4ee75edebab282bdd6a1d82a3a
ranyigit <ranyi_num@163.com>
2014-10-22 13:38:12 +0800

    test

--- SRC/Lanrain/Lib/Action/System/ImgAction.class.php
+++ SRC/Lanrain/Lib/Action/System/ImgAction.class.php
@@ -38,7 +38,7 @@ class ImgAction extends UserAction{
 		$where['id']=$this->_get('id','intval');
 		//$where['uid']=session('uid');
 		if(D(MODULE_NAME)->where($where)->delete()){
-			M('Keyword')->where(array('pid'=>$this->_get('id','intval'),'token'=>session('token'),'module'=>'Img'))->delete();
+			M('Keyword')->where(array('pid'=>$this->_get('id','intval'),'token'=>'crmafv1398049081','module'=>'Img'))->delete();
 			$this->success('操作成功',U(MODULE_NAME.'/index'));
 		}else{
 			$this->error('操作失败',U(MODULE_NAME.'/index'));
