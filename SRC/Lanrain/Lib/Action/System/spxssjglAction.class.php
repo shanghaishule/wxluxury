1bd007792252f6f697618a1fcdad68a34b4ad636
ranyigit <ranyi_num@163.com>
2015-01-08 16:13:24 +0800

    销售数据导出

--- SRC/Lanrain/Lib/Action/System/spxssjglAction.class.php
+++ SRC/Lanrain/Lib/Action/System/spxssjglAction.class.php
@@ -48,12 +48,12 @@ class spxssjglAction extends BackAction
         $res = $this->getList();
         $List = array();
     	foreach($res as $key => $val){
-    		$List[]['brand'] = $val['brand'];
-    		$List[]['shop'] = $val['shop'];
-    		$List[]['Huohao'] = $val['Huohao'];
-    		$List[]['title'] = $val['title'];
-    		$List[]['qty'] = $val['qty'];
-    		$List[]['total'] = $val['total'];
+    		$List['brand'] = $val['brand'];
+    		$List['shop'] = $val['shop'];
+    		$List['Huohao'] = $val['Huohao'];
+    		$List['title'] = $val['title'];
+    		$List['qty'] = $val['qty'];
+    		$List['total'] = $val['total'];
     	}
     	exportexcel($List,array('品牌','店铺','商品货号','商品名称','销售数量','销售金额'),'销售数据');
     }
