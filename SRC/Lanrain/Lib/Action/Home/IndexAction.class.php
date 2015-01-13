ba51d303593a53af1c7087d30489d6defcbbfc72
ranyigit <ranyi_num@163.com>
2015-01-13 16:43:28 +0800

    商家忘记密码

--- SRC/Lanrain/Lib/Action/Home/IndexAction.class.php
+++ SRC/Lanrain/Lib/Action/Home/IndexAction.class.php
@@ -21,6 +21,59 @@ class IndexAction extends BaseAction{
 		$this->display();
 	}
 	
+	//忘记密码
+	public function forget(){
+	       if(IS_POST){
+					$code = $this->_post('code','trim');
+					$username = $this->_post('username','trim');
+					if($code == $_SESSION['code']){
+							if(!isset($_SESSION['Uphone'])){
+								 $this->error("验证码已过期");
+							}
+							$userInfo = M('application')->where(array('uname'=>$username))->find();
+							if($userInfo['phone'] == session('Uphone')){
+								$sms_url = "http://api.weimi.cc/2/sms/send.html";
+								$uid = "7Q30scwRSEyT";
+								$sms_password = "k3a2bzn7";//密码
+								$cid = 'giG8M3gmNUTw';//模板id
+								$Msg = new SendMsg($sms_url, $uid, $sms_password,$cid);
+								$returnMsg = $Msg ->sendAshop(session('Uphone'), $userInfo['uname'], $userInfo['password']);
+								$this->success("用户名密码已发送至您的手机！");
+							}else{
+								$this->error("验证码已过期");
+							}
+
+					}else{
+						$this->error('验证码错误！');
+					}
+	       }else{
+	       		$this->display();
+	       }  
+	}
+	
+	//发送验证码
+	public function sendCode(){
+		$username = $this->_post('username','trim');
+		$flag = M('application')->where(array('uname'=>$username))->find();
+		if(false == $flag || $flag==''){
+			 echo '0';
+		}else{
+			$code = '';
+			for($i=1;$i<=6;$i++){
+				$code.= strval(rand(0, 9));
+			}
+			session('code',$code);
+	    	$sms_url = "http://api.weimi.cc/2/sms/send.html";
+	    	$uid = "7Q30scwRSEyT";
+	    	$sms_password = "k3a2bzn7";//密码
+	    	$cid = 'D4M6SG46AX2y';//模板id
+	    	$Msg = new SendMsg($sms_url, $uid, $sms_password,$cid);
+	    	session('Uphone',$flag['phone']);
+	    	$Msg->sendsms($flag['phone'],$code);
+	    	echo '1';
+		}
+    			//$returnMsg = $Msg ->sendAshop($userIn['phone'], $userName,$password);
+	}
 	
 	public function test(){
 		require_once './Extend/PHPExcel_1.7.9/Classes/PHPExcel/IOFactory.php';
